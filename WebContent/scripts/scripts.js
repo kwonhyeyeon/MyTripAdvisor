a3fb8aadf381d6e3e511d1be2705ce837b572871
hyoseong3291 <wjdgytjd3291@gmail.com>
2019-06-24 16:09:00 +0900

    index1

---
+++ WebContent/scripts/scripts.js
@@ -0,0 +1,12 @@
+$(function () {
+    $(window).scroll(function () {
+        var top = $(window).scrollTop();
+
+        if (top > 0) {
+            $('#header').addClass('inverted');
+        } else {
+            $('#header').removeClass('inverted');
+        }
+    });
+    $(window).trigger('scroll');
+});
